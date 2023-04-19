9f6ba30f21a9549c6e1fae2a564114a50f6f4729
Athena Shikata <99115384+AthenaShikata@users.noreply.github.com>
2023-04-19 10:06:33 -0700

    Create mycolor4.sh

---
+++ PiCode/mycolor4.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "$(tput setaf 2)$(tput setab 0) 0 $(tput setaf 0)$(tput setab 7)00 $(tput setaf 7)$(tput setab 5) 0 $(tput setaf 7)$(tput setab 2) 0 $(tput setaf 0)$(tput setab 7) 00$(tput setaf 5)$(tput setab 0)000"
+echo "$(tput setaf 2)$(tput setab 0)0 0$(tput setaf 0)$(tput setab 7)0 0$(tput setaf 7)$(tput setab 5)0 0$(tput setaf 7)$(tput setab 2)0 0$(tput setaf 0)$(tput setab 7)0  $(tput setaf 5)$(tput setab 0)0  "
+echo "$(tput setaf 2)$(tput setab 0)000$(tput setaf 0)$(tput setab 7)00 $(tput setaf 7)$(tput setab 5)0 0$(tput setaf 7)$(tput setab 2)000$(tput setaf 0)$(tput setab 7)0  $(tput setaf 5)$(tput setab 0)00 "
+echo "$(tput setaf 2)$(tput setab 0)0 0$(tput setaf 0)$(tput setab 7)00 $(tput setaf 7)$(tput setab 5)0 0$(tput setaf 7)$(tput setab 2)0 0$(tput setaf 0)$(tput setab 7)0  $(tput setaf 5)$(tput setab 0)0  "
+echo "$(tput setaf 2)$(tput setab 0)0 0$(tput setaf 0)$(tput setab 7)0 0$(tput setaf 7)$(tput setab 5) 0 $(tput setaf 7)$(tput setab 2)0 0$(tput setaf 0)$(tput setab 7) 00$(tput setaf 5)$(tput setab 0)000"
