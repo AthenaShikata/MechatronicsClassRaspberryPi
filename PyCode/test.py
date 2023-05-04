0a2ff8298ac23ffeeaae467d618cacea7cd36fd4
Athena Shikata <99115384+AthenaShikata@users.noreply.github.com>
2023-05-04 10:22:45 -0700

    Update test.py

--- PyCode/test.py
+++ PyCode/test.py
@@ -1,1 +1,12 @@
-print(hex('FF'))
+import time, sys
+
+request = 'FF00A7'
+request = request.strip('\n#')
+request = hex(int(request,16))
+print(request)
+print(type(request))
+
+R=request[0,2]
+G=request[2,4]
+B=request[4,6]
+print(R,G,B)
