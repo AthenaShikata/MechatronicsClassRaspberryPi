0307caade0149f260bb04d79c237772ed3d0e977
Athena Shikata <99115384+AthenaShikata@users.noreply.github.com>
2023-05-04 08:48:26 -0700

    Update 2023-05-04 Multicolor LED.py

--- PyCode/2023-05-04 Multicolor LED.py
+++ PyCode/2023-05-04 Multicolor LED.py
@@ -3,15 +3,19 @@ import RPi.GPIO as GPIO
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7,GPIO.OUT)
-GPIO.setup(5,GPIO.OUT)
-GPIO.setup(3,GPIO.OUT)
 
-redPin = 7
-greenPin = 5
-bluePin = 3
+R = 7
+G = 5
+B = 3
+PINS = [R,G,B]
+GPIO.setup(PINS, GPIO.OUT, initial=GPIO.LOW)
 
-while(True):
-    GPIO.output(redPin,1)
-    GPIO.output(greenPin,1)
-    GPIO.output(bluePin,1)
+GPIO.output(R,1)
+GPIO.output(G,1)
+GPIO.output(B,1)
+while True:
+    request = raw_input("Enter RGB: ")
+    if len(request) == 3:
+        GPIO.output(R,int(request[0]))
+        GPIO.output(G,int(request[1]))
+        GPIO.output(B,int(request[2]))
