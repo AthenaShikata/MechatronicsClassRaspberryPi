dd1431edc2e9c790d053264cc79e45f0de9614ce
Athena Shikata <99115384+AthenaShikata@users.noreply.github.com>
2023-05-04 09:14:21 -0700

    2

--- PyCode/2023-05-04 Multicolor LED.py
+++ PyCode/2023-05-04 Multicolor LED.py
@@ -3,6 +3,9 @@ import RPi.GPIO as GPIO
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7,GPIO.OUT)
+GPIO.setup(5,GPIO.OUT)
+GPIO.setup(3,GPIO.OUT)
 
 R = 7
 G = 5
@@ -10,15 +13,7 @@ B = 3
 PINS = [R,G,B]
 GPIO.setup(PINS, GPIO.OUT, initial=GPIO.LOW)
 
-GPIO.output(R,1)
-GPIO.output(G,1)
-GPIO.output(B,1)
-try:
-    while True:
-        request = input("Enter RGB Hex: ")
-        if len(request) == 3:
-            GPIO.output(R,int(request[0]))
-            GPIO.output(G,int(request[1]))
-            GPIO.output(B,int(request[2]))
-except KeyboardInterrupt:
-    GPIO.cleanup()
+while(True):
+    GPIO.output(R,1)
+    GPIO.output(G,1)
+    GPIO.output(B,1)
