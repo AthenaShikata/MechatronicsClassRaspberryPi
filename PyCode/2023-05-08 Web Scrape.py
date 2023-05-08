8b9b2ff61b7140f4db52acb5302095d09be0f342
Athena Shikata <99115384+AthenaShikata@users.noreply.github.com>
2023-05-08 08:37:12 -0700

    Update 2023-05-08 Web Scrape.py

--- PyCode/2023-05-08 Web Scrape.py
+++ PyCode/2023-05-08 Web Scrape.py
@@ -1,5 +1,6 @@
 import requests
 #first parameter of the 'get' method is the 'url':
-url = requests.get('https://w3schools.com/python/demopage.htm')
+url = 'https://w3schools.com/python/demopage.htm'
+x = requests.get(url)
 #print the response text (the content of the requested file):
-print(url.text)
+print(x.text)
