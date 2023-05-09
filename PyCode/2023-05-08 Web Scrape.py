31c41e0e2d7ac6725dbd239d861e67feca3a071a
Athena Shikata <99115384+AthenaShikata@users.noreply.github.com>
2023-05-09 10:20:14 -0700

    s

--- PyCode/2023-05-08 Web Scrape.py
+++ PyCode/2023-05-08 Web Scrape.py
@@ -1,7 +1,9 @@
 import requests
 #first parameter of the 'get' method is the 'url':
-url = 'https://www.steamclown.org/projects/red_pill.html'
+url = 'https://www.swimmingrank.com/cal/strokes/strokes_pc/EEVHAAPFE_meets.html'
 # url = 'https://www.steamclown.org/projects/red_pill.txt'
 x = requests.get(url)
 #print the response text (the content of the requested file):
 print(x.text)
+print('')
+print(x.find('Latest Meets'))
