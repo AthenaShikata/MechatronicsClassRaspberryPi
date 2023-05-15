d749e456f7b9da3edb223728e26be5c492124ddb
Athena Shikata <99115384+AthenaShikata@users.noreply.github.com>
2023-05-15 07:44:51 -0700

    Update 2023-05-04 Multicolor LED 3.py

--- PyCode/2023-05-04 Multicolor LED 3.py
+++ PyCode/2023-05-04 Multicolor LED 3.py
@@ -17,13 +17,14 @@ try:
     while True:
         request = input("Enter RGB Hex (ex '#FF00A7' or 'FF00A7'): ")
         request = request.strip('\n#')
-        R = request[0,2]
-        G = request[2,4]
-        B = request[4,6]
-        print(R,G,B)
-        ('''if len(request) == 6:
-            R, G, B = tuple(hex(request[i:i+2], 16) for i in (0, 2, 4))
-            print(R+G+B)''')
+        if len(request) == 6:
+            red = request[0,2]
+            green = request[2,4]
+            blue = request[4,6]
+            print(R,G,B)
+            GPIO.output(R,int(red))
+            GPIO.output(G,int(green))
+            GPIO.output(B,int(blue))
 except KeyboardInterrupt:
     GPIO.output(R,0)
     GPIO.output(G,0)
